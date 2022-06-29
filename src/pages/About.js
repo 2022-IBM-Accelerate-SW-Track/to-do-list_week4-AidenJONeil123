c129557f3adefd740f61cfa6e3311a4288717646
Brainiac73 <aiden@rocapps.com>
2022-06-29 18:23:01 -0400

    completed part 4

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic_name from "../assets/profile_pic.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+        <div>
+          <div class="split left">
+            <div className="centered">
+              <img git
+                className="profile_image"
+                // Image goes here
+                src = {profile_pic_name}
+                alt="Profile Pic"
+              ></img>
             </div>
           </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Aiden O'Neil</div>
+                <div className="brief_description">
+                  I like to design and build robots.
+                  <br></br>
+                  <br></br>
+                  I am a Computer Engineering Major at Clarkson University
+                  <br></br>
+                  <br></br>
+                  I am from Rochester, New York and I love to travel.
+                </div>
+              </div>
+            </div>
         </div>
       </div>
-    );
+    )
   }
 }
